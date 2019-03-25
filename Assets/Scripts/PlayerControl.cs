b70f02041420f2658188b4ed16396999f1728ff6
SarahHessel <sarah.phessel@gmail.com>
2019-03-25 09:24:21 -0300

    Preparando o sistema de particulas das moedas

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.CompareTag("Coin")){
+            SFXManager.instance.ShowCoinParticles(other.gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}

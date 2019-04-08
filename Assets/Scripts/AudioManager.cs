f717345dedeb35b061a8ee67fa848868be65e207
SarahHessel <sarah.phessel@gmail.com>
2019-04-08 10:45:01 -0300

    FatBird animado

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -16,8 +16,5 @@ public class AudioManager : MonoBehaviour
     public void PlayCoinPickupSound(GameObject obj){
         AudioSource.PlayClipAtPoint(audioSFX.coinPickup, obj.transform.position);
     }
-    public void PlayJumpSound(GameObject obj){
-        AudioSource.PlayClipAtPoint(audioPlayer.jump, obj.transform.position);
-    }
 
 }

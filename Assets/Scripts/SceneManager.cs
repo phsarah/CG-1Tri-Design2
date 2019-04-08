b42c2eda6d766940189ae7544534d4e8091de8e0
SarahHessel <sarah.phessel@gmail.com>
2019-04-08 09:08:46 -0300

    Criando a classe UIComponents e fazendo as referencias

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -9,6 +9,8 @@ public class SceneManager : MonoBehaviour
     [SerializeField]
     private Transform Tape = null;
 
+    public UIComponents uiComponents;
+
     void Awake(){
         Assert.IsNotNull(Tape);
     }

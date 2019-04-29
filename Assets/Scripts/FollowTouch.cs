5d1883fdd6c8f4e1ca1270deffa548bd59e2628d
SarahHessel <sarah.phessel@gmail.com>
2019-04-29 00:37:13 -0300

    Terminando o FollowTouch

--- Assets/Scripts/FollowTouch.cs
+++ Assets/Scripts/FollowTouch.cs
@@ -6,13 +6,18 @@ public class FollowTouch : MonoBehaviour
 {
     public GameObject particles;
 
+    public float lerpRate = 4f;
+
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1"))
         {
             Vector3 aux = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 position = new Vector3(aux.x, aux.y, 0);
-            Instantiate(particles, position, Quaternion.identity);
+            Vector3 initialPosition = transform.position;
+            Vector3 finalPosition = new Vector3(aux.x, transform.position.y, 0);
+
+            transform.position = Vector3.Lerp(initialPosition, finalPosition, lerpRate * Time.deltaTime);
+
         }
     }
 }

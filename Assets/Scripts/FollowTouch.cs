050c84f32ab4c3b38eee8ccbbf33f4c30aac48d3
SarahHessel <sarah.phessel@gmail.com>
2019-04-29 00:12:01 -0300

    Comecando FollowTouch - pegando a posicao do clique

---
+++ Assets/Scripts/FollowTouch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTouch : MonoBehaviour
+{
+    public GameObject particles;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 aux = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 position = new Vector3(aux.x, aux.y, 0);
+            Instantiate(particles, position, Quaternion.identity);
+        }
+    }
+}
